54a6cba90c109b76ab87ba62771a67efc0a0ef9d
Juan Ortiz-Acosta <jdoa2003@gmail.com>
2023-08-02 10:15:14 -0600

    finished lab

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+document.addEventListener(`DOMContentLoaded`, function () {
+    console.log(`The DOM has Loaded`);
+    document.querySelector(`#text`).textContent = `This is really cool!`;
+})
